6341249e33a1397cee4edecd5da422d384163fed
Pathmasri E <pathmasri1372005@gmail.com>
2025-10-27 19:15:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-//your code here!
+const list = document.getElementById("infi-list")
+function addItems(num){
+	for(let i=0;i<num;i++){
+	    const li = document.createElement("li");
+			li.textContent=`item${++itemCount}`;
+	  list.appendChild(li);
+	}
+}
+addItems(10);
+window.addEventListener("scroll",()=>{
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+    addItems(2);
+  }
+});
 
